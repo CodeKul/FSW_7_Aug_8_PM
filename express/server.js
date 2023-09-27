d952d287d6fd0be74ad8040b0dc40a67edd25e1f
[ckcodecamp] <soniharsha47@gmail.com>
2023-09-27 21:30:55 +0530

    day33

--- express/server.js
+++ express/server.js
@@ -11,7 +11,35 @@ app.get('/',(req,res)=>{
     // res.status(505)
     // res.json({"msg":"Hello Budddy!!"});
     res.status(400).sendFile(__dirname+'/index.html');
-})
+});
+
+app.get('/about',(req,res)=>{
+    res.send('About us page');
+});
+
+// Routing parameter
+// How to define route params -> /:ParameterName
+app.get('/api/log/stuff/ad/log/:uname/:passw',(req,res)=>{
+    
+    // Admin & jds3edmd
+
+    // How to fetch param data
+    var username=req.params.uname;
+    var passw=req.params.passw;
+    if(username=="Admin" & passw=='jds3edmd' ){
+        // res.send('Admin logged successful')
+        res.sendFile(__dirname+'/admin.html');
+    }
+    else{
+        res.send('Sorry u r unable to log');
+    }
+});
+
+
+// render 404 page using all()
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>404 Page not Found !! Sorry !!!</h1> <a href="/">BAck to home page</a> ');
+});
 
 // listen -> It defines port number
 app.listen(8070,(err)=>{
